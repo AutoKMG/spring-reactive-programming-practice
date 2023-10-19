98ed66eeda69ed4005ac1a0f1b9b3a4d477baded
Khaled Saleh <th.be03@gmail.com>
2023-10-19 23:26:09 +0200

    Minor renaming

--- student-app/src/main/java/com/khaledsaleh/studentsapp/StudentAppApplication.java
+++ student-app/src/main/java/com/khaledsaleh/studentsapp/StudentAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentsAppApplication {
+public class StudentAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentsAppApplication.class, args);
+		SpringApplication.run(StudentAppApplication.class, args);
 	}
 
 }
