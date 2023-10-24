0c1fadddbcea9803e1f31d70d7ad7f87e26cfd31
Khaled Saleh <th.be03@gmail.com>
2023-10-24 21:26:40 +0200

    Migrating aircraft positions to RSocket

--- aircraft-positions/src/main/java/com/khaledsaleh/aircraftpositions/PositionController.java
+++ aircraft-positions/src/main/java/com/khaledsaleh/aircraftpositions/PositionController.java
@@ -1,18 +1,28 @@
 package com.khaledsaleh.aircraftpositions;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
-@RequiredArgsConstructor
 @Controller
 public class PositionController {
     private final AircraftRepository repository;
+    private final RSocketRequester requester;
     private final WebClient client = WebClient.create("http://localhost:7634/aircraft");
 
+    public PositionController(AircraftRepository repository, RSocketRequester.Builder builder){
+        this.repository = repository;
+        this.requester = builder.tcp("localhost", 7635);
+    }
+
+    // HTTP Requester
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model){
         Flux<Aircraft> aircraftFlux = repository.deleteAll()
@@ -24,4 +34,14 @@ public class PositionController {
         model.addAttribute("currentPositions", aircraftFlux);
         return "positions";
     }
+
+    // RSocket client endpoint
+    @ResponseBody
+    @GetMapping(value = "/acstream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Aircraft> getCurrentACPositionStream(){
+        return requester.route("acstream")
+                .data("Requesting aircraft positions")
+                .retrieveFlux(Aircraft.class);
+    }
+
 }
