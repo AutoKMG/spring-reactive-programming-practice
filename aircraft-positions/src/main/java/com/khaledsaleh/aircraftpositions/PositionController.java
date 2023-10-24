6022cb2beb5f37d7e9c2f50bd83a4505888cd444
Khaled Saleh <th.be03@gmail.com>
2023-10-24 20:31:40 +0200

    Refactoring the controller to non-blocking approach

--- aircraft-positions/src/main/java/com/khaledsaleh/aircraftpositions/PositionController.java
+++ aircraft-positions/src/main/java/com/khaledsaleh/aircraftpositions/PositionController.java
@@ -1,21 +1,27 @@
 package com.khaledsaleh.aircraftpositions;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @RequiredArgsConstructor
 @Controller
 public class PositionController {
-    @NonNull
     private final AircraftRepository repository;
+    private final WebClient client = WebClient.create("http://localhost:7634/aircraft");
 
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model){
-        model.addAttribute("currentPositions", repository.findAll());
+        Flux<Aircraft> aircraftFlux = repository.deleteAll()
+                        .thenMany(client.get()
+                                .retrieve()
+                                .bodyToFlux(Aircraft.class)
+                                .filter(plane -> !plane.getReg().isEmpty())
+                                .flatMap(repository::save));
+        model.addAttribute("currentPositions", aircraftFlux);
         return "positions";
     }
 }
