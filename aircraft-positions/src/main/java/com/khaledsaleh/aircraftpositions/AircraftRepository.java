6db6bc106aaaa755c0863264d2f5e58839b47579
Khaled Saleh <th.be03@gmail.com>
2023-10-24 19:22:24 +0200

    Updating repo and pom.xml and removing project old version classes

--- aircraft-positions/src/main/java/com/khaledsaleh/aircraftpositions/AircraftRepository.java
+++ aircraft-positions/src/main/java/com/khaledsaleh/aircraftpositions/AircraftRepository.java
@@ -1,6 +1,6 @@
 package com.khaledsaleh.aircraftpositions;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface AircraftRepository extends CrudRepository<Aircraft, Long> {
+public interface AircraftRepository extends ReactiveCrudRepository<Aircraft, Long> {
 }
